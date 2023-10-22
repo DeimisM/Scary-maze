1331f03d27bef41b966d620255a22aa0ae429d52
DeimisM <deimis.mardosas@outlook.com>
2023-10-22 17:35:21 +0300

    Finished the game

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -9,5 +10,13 @@ public class Player : MonoBehaviour
         var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePos.z = 0;
         GetComponent<Rigidbody2D>().MovePosition(mousePos);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name.Contains("Wall"))
+        {
+            SceneManager.LoadScene("Menu");
+        }
     }
 }
